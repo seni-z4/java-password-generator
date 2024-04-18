f9bd013f4c6ec91586bafa7ed34a5255cb51f8b2
seniya <seniyathri@gmail.com>
2024-04-18 23:12:36 +0200

    feat; the password has been created
    the password has been created and its generating the expected values

---
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -0,0 +1,45 @@
+package org.lessons.java.security;
+
+import java.util.Scanner;
+
+public class PasswordGenerator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("#inzio la generazione della programma#");
+		
+		/*creare dei variabli nome cognome colore data di nascita(giorni,mesi,anno)
+		 * 
+		 * poi stapare il nome, cognome, colore, somma della data di nascita 
+		 * 
+		 * 
+		 * */
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("inserisci il tuo nome");
+		String nome = input.nextLine();
+		
+		System.out.println("inserici il tuo cognome");
+		String cognome = input.nextLine();
+		
+		System.out.println("inserici il tuo colore preferito");
+		String colore = input.nextLine();
+		
+		System.out.println("inserici il tuo giorno di nascita");
+		int giorno = input.nextInt();
+		
+		System.out.println("inserici il tuo mese di nascita");
+		int mese = input.nextInt();
+		
+		System.out.println("inserici il tuo anno di nascita");
+		int anno = input.nextInt();
+		
+		
+		
+		System.out.println(nome + cognome + colore + giorno + mese + anno);
+		
+	}
+	
+}
+		
